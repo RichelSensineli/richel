b26530a19c1ffe54b106a50e2e15ff4a9b9097d6
Richel Sensineli <richelsensineli@gmail.com>
2019-03-20 20:38:38 -0300

    First Commit - Project Setup

---
+++ src/main/java/io/richel/curso/RichelApplication.java
@@ -0,0 +1,13 @@
+package io.richel.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/richel/curso/RichelApplicationTests.java
@@ -0,0 +1,16 @@
+package io.richel.curso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RichelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
