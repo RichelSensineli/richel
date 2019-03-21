737fd1798565122f39c875784075cac5d5de28d3
Richel Sensineli <richelsensineli@gmail.com>
2019-03-21 20:38:07 -0300

    Using CommandLineRunner Java class

--- src/main/java/io/richel/curso/RichelApplication.java
+++ src/main/java/io/richel/curso/RichelApplication.java
@@ -1,13 +1,33 @@
 package io.richel.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RichelApplication {
+import io.richel.curso.domain.Categoria;
+import io.richel.curso.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class RichelApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RichelApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
