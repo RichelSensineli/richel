9315b0c8fadcc424e607fb5691a2340ef6703a7d
Richel Sensineli <richelsensineli@gmail.com>
2019-03-20 22:59:08 -0300

    Adding H2 database and Categoria table

--- src/main/java/io/richel/curso/domain/Categoria.java
+++ src/main/java/io/richel/curso/domain/Categoria.java
@@ -2,10 +2,19 @@ package io.richel.curso.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	private static final long serialVersionUID = -4868482734552690305L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
