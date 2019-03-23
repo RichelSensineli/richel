03387882f83456e65d1845a2a135a0c2c98a92ed
Richel Sensineli <richelsensineli@gmail.com>
2019-03-22 23:31:25 -0300

    Adding Pedidos end-point

---
+++ src/main/java/io/richel/curso/services/PedidoService.java
@@ -0,0 +1,24 @@
+package io.richel.curso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.richel.curso.domain.Pedido;
+import io.richel.curso.repositories.PedidoRepository;
+import io.richel.curso.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> objeto = repository.findById(id);
+		
+		return objeto.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
