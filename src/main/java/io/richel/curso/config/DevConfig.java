53a50080ccfcecb2df3724e919252573abfbb22d
Richel Sensineli <richelsensineli@gmail.com>
2019-03-26 20:09:48 -0300

    Adding Send E-Mail functionality

--- src/main/java/io/richel/curso/config/DevConfig.java
+++ src/main/java/io/richel/curso/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import io.richel.curso.services.DBService;
+import io.richel.curso.services.EmailService;
+import io.richel.curso.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.InstatiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
