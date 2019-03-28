6df6177ecc95ad7b5428d2766d20d0dbca693dab
Richel Sensineli <richelsensineli@gmail.com>
2019-03-27 22:32:27 -0300

    Implementing Authorization logic

--- src/main/java/io/richel/curso/config/SecurityConfig.java
+++ src/main/java/io/richel/curso/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import io.richel.curso.security.JWTAuthenticationFilter;
+import io.richel.curso.security.JWTAuthorizationFilter;
 import io.richel.curso.security.JWTUtil;
 
 @Configuration
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
